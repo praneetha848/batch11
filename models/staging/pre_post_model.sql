2b5882d83d7fcb15e098de4865b2bf0a70359393
sai sri <snowflaketraining48@gmail.com>
2025-07-28 11:38:19 +0000

    commit

--- models/staging/pre_post_model.sql
+++ models/staging/pre_post_model.sql
@@ -1,10 +1,11 @@
-{{ config(
-    materialized="table",
-    pre_hook="delete * from  {{this}} where USER_ID =null", 
-    post_hook="TRUNCATE TABLE {{ref('orders')}}"
-) }}
+{{
+    config(
+        materialized="table",
+        pre_hook="delete  from  {{this}} where USER_ID =null",
+        post_hook="TRUNCATE TABLE {{ref('orders')}}",
+    )
+}}
 
-WITH tb1 as(
-  select 
- * from {{ref('orders')}})
-select * from tb1
+with tb1 as (select * from {{ ref("orders") }})
+select *
+from tb1
