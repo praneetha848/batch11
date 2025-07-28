e485701bd60ba554cfd9b61e08a24ae662987f01
sai sri <snowflaketraining48@gmail.com>
2025-07-28 12:27:29 +0000

    commit

---
+++ models/staging/compare_table_rowcount.sql
@@ -0,0 +1,3 @@
+
+{{ default__compare_row_counts
+('SALES.DBT_DEV.CUSTOMERS','SALES.DBT_DEV.ORDERS','SALES.DBT_DEV.EMP_SEED')}}
--- models/staging/order_append.sql
+++ models/staging/order_append.sql
@@ -3,4 +3,5 @@
     incremental_strategy="append",
 ) }}
 
-select * from {{source('datafeed_shared_schema','raw_order')}} 
+select * from {{source('datafeed_shared_schema','raw_order')}}  
+where id in (9,6,7,8)
